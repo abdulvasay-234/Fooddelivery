b768636a243c3b5f839589029489b40a0e89ddc6
Afnan Abdul Vasay <afnanabdulvasay234@gmail.com>
2023-08-18 23:13:20 +0530

    Update delivery.py

--- Time prediction/delivery.py
+++ Time prediction/delivery.py
@@ -35,4 +35,88 @@ numerical_columns = df.select_dtypes(include=['int64', 'float64']).columns
 print("Numerical Columns:")
 print(numerical_columns)
 
-#13
+
+df['Type_of_order'].unique()
+
+df['Type_of_order'].value_counts()
+
+plt.figure(figsize=(15,6))
+sns.countplot(x='Type_of_order', data=df, palette='hls')
+plt.xticks(rotation=0)
+plt.show()
+
+plt.figure(figsize=(15, 6))
+counts = df['Type_of_order'].value_counts()
+plt.pie(counts, labels=counts.index, autopct='%1.1f%%', colors=sns.color_palette('hls'))
+plt.title('Type_of_order')
+plt.show()
+
+import plotly.graph_objects as go
+
+fig = go.Figure(data=[go.Bar(x=df['Type_of_order'].value_counts().index, y=df['Type_of_order'].value_counts())])
+fig.update_layout(
+        title= 'Type_of_order',
+        xaxis_title="Categories",
+        yaxis_title="Count"
+    )
+fig.show()
+
+counts = df['Type_of_order'].value_counts()
+fig = go.Figure(data=[go.Pie(labels=counts.index, values=counts)])
+fig.update_layout(title= 'Type_of_order')
+fig.show()
+
+df['Type_of_vehicle'].unique()
+
+df['Type_of_vehicle'].value_counts()
+
+plt.figure(figsize=(15,6))
+sns.countplot(x='Type_of_vehicle', data=df, palette='hls')
+plt.xticks(rotation=0)
+plt.show()
+
+plt.figure(figsize=(15, 6))
+counts = df['Type_of_vehicle'].value_counts()
+plt.pie(counts, labels=counts.index, autopct='%1.1f%%', colors=sns.color_palette('hls'))
+plt.title('Type_of_vehicle')
+plt.show()
+
+fig = go.Figure(data=[go.Bar(x=df['Type_of_vehicle'].value_counts().index, y=df['Type_of_vehicle'].value_counts())])
+fig.update_layout(
+        title= 'Type_of_vehicle',
+        xaxis_title="Categories",
+        yaxis_title="Count"
+    )
+fig.show()
+
+
+counts = df['Type_of_vehicle'].value_counts()
+fig = go.Figure(data=[go.Pie(labels=counts.index, values=counts)])
+fig.update_layout(title= 'Type_of_vehicle')
+fig.show()
+
+for i in numerical_columns:
+    plt.figure(figsize=(15,6))
+    sns.histplot(df[i], kde = True, bins = 20, palette = 'hls')
+    plt.xticks(rotation = 90)
+    plt.show()
+for i in numerical_columns:
+    plt.figure(figsize=(15,6))
+    sns.distplot(df[i], kde = True, bins = 20)
+    plt.xticks(rotation = 90)
+    plt.show()
+  
+for i in numerical_columns:
+    plt.figure(figsize=(15, 6))
+    sns.boxplot(x=i, data=df, palette='hls')
+    plt.xticks(rotation=90)
+    plt.show()
+
+for i in numerical_columns:
+    plt.figure(figsize=(15,6))
+    sns.violinplot(x = i, data = df, palette = 'hls')
+    plt.xticks(rotation = 90)
+    plt.show()
+
+#30
+
